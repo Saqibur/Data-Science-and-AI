aa836dde9e277ec36cd864ed416c29e1b69db85b
Saqibur <saqibur.rahman98@gmail.com>
2019-06-14 21:57:30 +0600

    Reorganize folders

---
+++ Deep Learning with Python Practice/Handwritten-Number-Recognition/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+new_model = tf.keras.models.load_model('Handwritten-Numbers-Recognition/Models/number_reader.model')
+predictions = new_model.predict(x_test)
+
+# Change the index, i, to some other number to change which is predicted via the model
+
+i = 21
+
+print(np.argmax(predictions[i]))
+
+plt.imshow(x_test[i])
+plt.show()
