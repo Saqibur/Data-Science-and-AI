6f4c1ec8ec4d7f3f1bda426a7ec6be1108721698
Saqibur <saqibur.rahman98@gmail.com>
2019-09-19 20:00:18 +0600

    Add basics.py

---
+++ Python Refresher/Basics.py
@@ -0,0 +1,9 @@
+list_of_numbers = [1, 2, 3, 4, 5]
+
+for number in list_of_numbers:
+    print(number)
+    if (number % 2 == 0):
+        print("is even.")
+    else:
+        print("is odd.")
+print("All done.")
